3af3e560192f148cdd5059e5bd1e0d359c6d165d
Trung Lam <44829001+Krazriel@users.noreply.github.com>
2022-04-26 13:48:23 -0700

    completed search algorithm

--- adjust_the_counters.cpp
+++ adjust_the_counters.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <queue>
 #include <cmath>
 
 using namespace std;
+
+vector< pair<int, vector<int> > > depthTable;
 
 vector<int> setState(string userInput){
     vector<int> temp;
@@ -18,6 +21,7 @@ vector<int> setState(string userInput){
 
 vector< vector<int> > expandNode(vector<int> state){
     vector< vector<int> > result;
+    int depth = 0;
     for(int i = 0; i < state.size(); i++){
         for(int j = 0; j < state.size(); j++){
             vector<int> temp = state;
@@ -29,27 +33,33 @@ vector< vector<int> > expandNode(vector<int> state){
         }
         
     }
+    //remove duplicates
+    sort(result.begin(), result.end());
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+
+    //retrieve depth of parent
+    for(int i = 0; i < depthTable.size(); i++){
+        if(depthTable[i].second == state){
+            depth = depthTable[i].first;
+            break;
+        }
+    }
+
+    depth += 1;
+    for(int i = 0; i < result.size(); i++){
+        depthTable.push_back(make_pair(depth, result[i]));
+    }
+
     return result;
 }
 
-int getDepth(vector<int> node, vector<int> target){
-    bool stateFound = false;
+int getDepth(vector<int> node){
     int depth = 0;
-    vector< vector<int> > tempNodes;
-    vector< vector<int> > currNodes;
-    tempNodes.push_back(node);
-    while(!stateFound){
-        depth++;
-        for(int i = 0; i < tempNodes.size(); i++){
-            currNodes = expandNode(tempNodes[i]);
-            for(int i = 0; i < currNodes.size(); i++){
-                if(currNodes[i] == target){
-                    return depth;
-                }
-            }
+    for(int i = 0; i < depthTable.size(); i++){
+        if(depthTable[i].second == node){
+            depth = depthTable[i].first;
+            return depth;
         }
-        tempNodes = currNodes;
-
     }
     return -1;
 }
@@ -64,6 +74,7 @@ void printMatrix(vector<int> node, int matrixSize){
         }
         cout << endl;
     }
+    cout << endl;
 }
 
 int misplacedHeuristic(vector<int> node){
@@ -82,12 +93,12 @@ int misplacedHeuristic(vector<int> node){
     return score;
 }
 
-int aStar(vector<int> node, vector<int> target, int heuristic){
+int aStar(vector<int> node, int heuristic){
     if(heuristic == 0){
-        return getDepth(node, target);
+        return getDepth(node);
     }
     else{
-        return misplacedHeuristic(target) + getDepth(node, target);
+        return misplacedHeuristic(node) + getDepth(node);
     }
 }
 
@@ -107,6 +118,66 @@ bool goalCheck(vector<int> node){
     return true;
 }
 
+void generalSearch(vector<int> initState){
+
+    //Store repeating nodes
+    vector< vector<int> > repeatedNodes;
+
+    //check if initial state is goal state
+    if(goalCheck(initState)){
+        cout << "Success: Found Solution:" << endl;
+        printMatrix(initState, sqrt(initState.size()));
+        return;
+    }
+
+    //Expand Node and insert to queue
+    vector< vector<int> > nodes = expandNode(initState);
+
+    //Start Loop
+    while(1){
+        //Sort queue by A_Star w/ Misplaced Heuristic (1)
+
+        cout << "Sorting..." << endl;
+        vector< pair<int, vector<int> > > nodesTable;
+        for(int i = 0; i < nodes.size(); i++){
+            nodesTable.push_back(make_pair(aStar(nodes[i], 1), nodes[i]));
+        }
+        sort(nodesTable.begin(), nodesTable.end());
+        cout << "Sorting Complete!" << endl;
+
+        vector< vector<int> > sortedNodes;
+
+        for(int i = 0; i < nodesTable.size(); i++){
+            sortedNodes.push_back(nodesTable[i].second);
+        }
+
+
+        // if queue is empty
+        if(sortedNodes.size() == 0){
+            cout << "Failure: Unable to Find Solution!" << endl;
+            return;
+        }
+
+        //Pop front of element
+        vector<int> node = sortedNodes.front();
+        sortedNodes.erase(sortedNodes.begin());
+        cout << "g(n) : " << getDepth(node) << " , h(n) : " << misplacedHeuristic(node) << endl;
+        printMatrix(node, sqrt(node.size()));
+        if(goalCheck(node)){
+            cout << "Success: Found Solution:" << endl;
+            printMatrix(node, sqrt(node.size()));
+            return;
+        }
+
+        //Expand and add to queue
+        vector< vector<int> > temp = expandNode(node);
+        for(int i = 0; i < temp.size(); i++){
+            nodes.push_back(temp[i]);
+        }
+    }
+
+}
+
 int main(){
     string userInput;
     int userChoice;
@@ -124,45 +195,16 @@ int main(){
         cout << "Enter counters from left to right, top to bottom seperated by space: ";
         getline(cin, userInput);
         initState = setState(userInput);
-        nSize = nSize = sqrt(initState.size());
+        nSize = sqrt(initState.size());
     }
 
-    vector< vector<int> > nodes = expandNode(initState);
+    //initialize depth table
+    depthTable.push_back(make_pair(0, initState));
 
-    sort(nodes.begin(), nodes.end());
-    nodes.erase(std::unique(nodes.begin(), nodes.end()), nodes.end());
-
-    vector< pair<int, vector<int> > > nodesTable;
-    for(int i = 0; i < nodes.size(); i++){
-        nodesTable.push_back(make_pair(aStar(initState, nodes[i], 1), nodes[i]));
-    }
-
-    sort(nodesTable.begin(), nodesTable.end());
-
-    //Debugger Section
-    cout << "Table Test:" << endl;
-    for(int i = 0; i < nodesTable.size(); i++){
-        for(int j = 0; j < nodesTable[i].second.size(); j++){
-            cout << nodesTable[i].second[j] << " ";
-        }
-        cout << "\ta(n) : " << nodesTable[i].first << endl;
-    }
-
-    cout << endl;
-
-    cout << "Expand Test:" << endl;
-    for(int i = 0; i < nodes.size(); i++){
-        for(int j = 0; j < nodes[0].size(); j++){
-            cout << nodes[i][j] << " ";
-        }
-        cout << " : g(n) = " << getDepth(initState, nodes[i]) 
-        << " , h(n) = " << misplacedHeuristic(nodes[i]) 
-        << " , a(n) = " << aStar(initState, nodes[i], 1) << endl;
-    }
-
-    cout << endl;
-    printMatrix(nodes[0], nSize);
-    //Debugger Section
+    //Start search
+    cout << "Initial State: " << endl;
+    printMatrix(initState, nSize);
+    generalSearch(initState);
 
     return 0;
 }
